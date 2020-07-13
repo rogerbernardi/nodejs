9b13b2e5aeee4c4721d83a16c6c442ba3d04e9a5
Roger Bernardi <rogerbernardi@gmail.com>
2020-07-13 20:48:32 -0300

    initial

---
+++ server.js
@@ -0,0 +1,18 @@
+/*jshint esversion: 6 */
+/*jshint maxerr: 10000 */
+/* eslint-disable no-console */
+/* jshint esversion: 8 */
+
+
+//create a server object:
+// var http = require('http');
+// var fs = require('fs');
+// http.createServer(function(req, res) {
+//     fs.readFile('./html/index.html', function(err, data) {
+//         res.writeHead(200, {
+//             'Content-Type': 'text/html'
+//         });
+//         res.write(data);
+//         return res.end();
+//     });
+// }).listen(8080);
