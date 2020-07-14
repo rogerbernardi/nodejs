9a58706251740d08487765a24b17becd59604ef3
Roger Bernardi <rogerbernardi@gmail.com>
2020-07-14 06:45:11 -0300

    pierre async/await

--- server.js
+++ server.js
@@ -16,3 +16,30 @@
 //         return res.end();
 //     });
 // }).listen(8080);
+
+
+
+function sum(x) {
+    return new Promise((resolve, reject) => {
+        if (Number(x) == NaN || Number(x) == undefined || typeof(x) != 'number') {
+            reject('Tá de brincadeira?');
+        }
+
+        setTimeout(() => {
+            resolve(x + 5000)
+        }, 3000);
+    })
+}
+
+async function main() {
+    try {
+        const resultado = await sum(5);
+        console.log(`Resultado com async await: ${resultado}`);
+    } catch (error) {
+        console.log(`Temos prolemas: ${error}`);
+        console.log(process.env.username);
+    }
+
+}
+
+main();
